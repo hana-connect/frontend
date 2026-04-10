bbff4939e38c3ac2a2b86402d54a106915b98142
hansoojeongsj <tnwjd3758@gmail.com>
2026-04-10 14:32:07 +0900

    feat(root): header 수정

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -21,52 +21,45 @@ const Header = ({
   const router = useRouter();
 
   if (type === "main") {
-    if (type === "main") {
-      return (
-        <header className="sticky top-0 z-50 flex h-15 w-full items-center justify-between bg-[#F5F5F5] px-4">
-          <div className="flex items-center">
-            <div className="relative h-10 w-10">
-              <Image
-                src="/svg/ic_logo_grey.svg"
-                alt="하나 커넥트 로고"
-                fill
-                style={{ objectFit: "contain" }}
-                priority
-              />
-            </div>
+    return (
+      <header className="sticky top-0 z-50 flex h-15 w-full items-center justify-between bg-[#F5F5F5] px-4">
+        <div className="flex items-center">
+          <div className="relative h-10 w-10">
+            <Image
+              src="/svg/ic_logo_grey.svg"
+              alt="하나 커넥트 로고"
+              fill
+              sizes="40px"
+              style={{ objectFit: "contain" }}
+              priority
+            />
           </div>
+        </div>
 
-          <div className="flex items-center gap-7 text-[#969DB4]">
-            <button
-              type="button"
-              aria-label="스캔"
-              className="relative h-6 w-6"
-            >
-              <ScanLine size={24} />
-            </button>
+        <div className="flex items-center gap-7 text-[#949AAD] pr-2">
+          <button type="button" aria-label="스캔" className="relative h-6 w-6">
+            <ScanLine size={24} />
+          </button>
 
-            <button
-              type="button"
-              aria-label="알림"
-              className="relative h-6 w-6"
-            >
-              <Image
-                src="/svg/ic_bell.svg"
-                alt="알림"
-                fill
-                style={{ objectFit: "contain" }}
-              />
-            </button>
-          </div>
-        </header>
-      );
-    }
+          <button type="button" aria-label="알림" className="relative h-6 w-6">
+            <Image
+              src="/svg/ic_bell.svg"
+              alt="알림"
+              fill
+              sizes="24px"
+              style={{ objectFit: "contain" }}
+            />
+          </button>
+        </div>
+      </header>
+    );
   }
 
   return (
-    <header className="sticky top-0 z-50 flex h-14 w-full items-center justify-center bg-white px-4 text-black">
+    <header className="sticky top-0 z-50 flex h-15 w-full items-center justify-center bg-white px-4 text-black">
       <button
         type="button"
+        aria-label="뒤로 가기"
         onClick={() => router.back()}
         className="absolute left-4 p-1"
       >
