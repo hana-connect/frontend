6e3c7e93b151d61a0bce31644274e4dc55238352
hansoojeongsj <tnwjd3758@gmail.com>
2026-04-10 17:22:20 +0900

    feat(root): style 변경

--- src/common/components/input/Input.tsx
+++ src/common/components/input/Input.tsx
@@ -4,18 +4,22 @@ import type React from "react";
 import { type ChangeEvent, useId } from "react";
 
 type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> & {
-  label: string;
+  label?: string;
   rightLabel?: string;
+  onRightLabelClick?: () => void;
   isNumber?: boolean;
 };
 
 const Input = ({
   label,
   rightLabel,
+  onRightLabelClick,
   isNumber = false,
   value,
   onChange,
   placeholder,
+  className = "",
+  type,
   ...props
 }: InputProps) => {
   const generatedId = useId();
@@ -29,28 +33,37 @@ const Input = ({
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (isNumber) {
       const formattedValue = formatNumber(e.target.value);
-
       e.target.value = formattedValue;
     }
-
     if (onChange) onChange(e);
   };
 
   return (
     <div className="w-full flex flex-col">
-      <div className="flex justify-between items-end mb-6">
-        <label
-          htmlFor={inputId}
-          className="text-[20px] font-bold text-black leading-none"
-        >
-          {label}
-        </label>
-        {rightLabel && (
-          <span className="text-[16px] text-[#8D839D] border-b border-[#8D839D] pb-px leading-none">
-            [{rightLabel}]
-          </span>
-        )}
-      </div>
+      {(label || rightLabel) && (
+        <div className="flex justify-between items-end mb-6">
+          {label ? (
+            <label
+              htmlFor={inputId}
+              className="text-[20px] font-bold text-black leading-none"
+            >
+              {label}
+            </label>
+          ) : (
+            <div />
+          )}
+
+          {rightLabel && (
+            <button
+              type="button"
+              onClick={onRightLabelClick}
+              className="text-[16px] text-[#8D839D] border-b border-[#8D839D] pb-px leading-none"
+            >
+              [{rightLabel}]
+            </button>
+          )}
+        </div>
+      )}
 
       <div className="relative flex flex-col">
         <input
@@ -59,8 +72,9 @@ const Input = ({
           value={value}
           onChange={handleChange}
           placeholder={placeholder}
+          type={isNumber ? "text" : type || "text"}
           inputMode={isNumber ? "numeric" : "text"}
-          className="w-full text-[20px] text-black placeholder:text-[#CCCCCC] outline-none border-b border-[#CCCCCC] pb-3 font-bold"
+          className={`w-full text-[20px] text-black placeholder:text-[#CCCCCC] outline-none border-b border-[#CCCCCC] pb-3 font-bold ${className}`}
         />
       </div>
     </div>
