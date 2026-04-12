fdfa46374da6f0b0c2974727a0ca27f925fbfe0e
thisminji <thisminji@dgu.ac.kr>
2026-04-12 15:04:11 +0900

    feat(wallet/register/register-intro): 계좌 등록하기 부모일 때 보이는 뷰 구현
    스타일이랑 색상 추가하면 변경해야함

--- src/app/wallet/register/_components/steps/register-intro/register-intro.tsx
+++ src/app/wallet/register/_components/steps/register-intro/register-intro.tsx
@@ -25,7 +25,7 @@ const introViewContent: Record<
 };
 
 const RegisterIntro = ({ onNext }: Props) => {
-  const viewType: ViewType = "child";
+  const viewType: ViewType = "parent";
 
   const { bannerTitle, showIrpCard } = introViewContent[viewType];
 
@@ -33,43 +33,58 @@ const RegisterIntro = ({ onNext }: Props) => {
     <>
       <Header type="sub" title="계좌 등록하기" />
       <RegisterInfoBanner title={bannerTitle} />
-      <div className="mt-9 px-6">
-        <button type="button" onClick={onNext} className="block w-full">
-          <Image
-            src={accountRegisterImage}
-            alt="계좌 등록 안내"
-            className="h-auto w-full"
-            priority
-          />
-        </button>
-      </div>
-      {showIrpCard && (
-        <div className="mt-6 px-6">
-          <IrpCard />
+      <div className="flex flex-1 flex-col">
+        <div className="mt-9 px-6">
+          <button type="button" onClick={onNext} className="block w-full">
+            <Image
+              src={accountRegisterImage}
+              alt="계좌 등록 안내"
+              className="h-auto w-full"
+              priority
+            />
+          </button>
         </div>
-      )}
-      {!showIrpCard && (
-        <div className="mt-14.25 px-6">
-          <div className="flex flex-col gap-6 text-left">
+        {showIrpCard && (
+          <div className="mt-2.75 px-6">
+            <p className="text-left text-[16px] text-black">
+              연금 계좌 등록하고 퀴즈 풀면
+              <br />
+              적립금 넣어드려요!
+            </p>
+          </div>
+        )}
+        {showIrpCard && (
+          <div className="mt-auto px-6 pb-9">
             <div className="flex flex-col gap-2">
-              <h2 className="text-[16px] text-black">충전계좌 등록 안내</h2>
-              <ul className="list-disc pl-6 text-[16px] text-[#555555]">
-                <li>입출금 계좌만 등록할 수 있어요.</li>
-                <li>만 14세 미만은 하나은행 계좌만 등록할 수 있어요.</li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-2 pt-1">
-              <p className="text-[16px] text-black">계좌가 없다면?</p>
-              <p className="text-[16px] text-[#555555]">
-                아이부자 앱에서{" "}
-                <span className="text-[#B465FF]">비대면 계좌개설</span>로
-                하나은행 계좌를 만들 수 있어요.
+              <IrpCard />
+              <p className="text-left text-[12px] text-[#555555]">
+                자세한 내용은 하나은행 퇴직연금 전용 고객센터 1599-2080
               </p>
             </div>
           </div>
-        </div>
-      )}
-      <div className="flex flex-1 flex-col gap-4 p-6"></div>
+        )}
+        {!showIrpCard && (
+          <div className="mt-14.25 px-6">
+            <div className="flex flex-col gap-6 text-left">
+              <div className="flex flex-col gap-2">
+                <h2 className="text-[16px] text-black">충전계좌 등록 안내</h2>
+                <ul className="list-disc pl-6 text-[16px] text-[#555555]">
+                  <li>입출금 계좌만 등록할 수 있어요.</li>
+                  <li>만 14세 미만은 하나은행 계좌만 등록할 수 있어요.</li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-2 pt-1">
+                <p className="text-[16px] text-black">계좌가 없다면?</p>
+                <p className="text-[16px] text-[#555555]">
+                  아이부자 앱에서{" "}
+                  <span className="text-[#B465FF]">비대면 계좌개설</span>로
+                  하나은행 계좌를 만들 수 있어요.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
