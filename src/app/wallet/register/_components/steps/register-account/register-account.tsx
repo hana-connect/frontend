a29bb5bb329950befeac328412cef198b4ca1dbd
thisminji <thisminji@dgu.ac.kr>
2026-04-12 16:40:26 +0900

    style(wallet/register/register-account): 디자인 시스템 반영

--- src/app/wallet/register/_components/steps/register-account/register-account.tsx
+++ src/app/wallet/register/_components/steps/register-account/register-account.tsx
@@ -19,13 +19,15 @@ const RegisterAccount = ({ onNext }: Props) => {
   return (
     <>
       <Header type="sub" title="계좌 등록하기" />
-      <div className="flex flex-1 flex-col px-6 pt-7 pb-9">
-        <h1 className="text-left text-[24px] leading-7.5 font-bold tracking-[-1.2px] text-brand-black">
-          하나은행 계좌번호를
-          <br />
-          입력해 주세요.
-        </h1>
-        <div className="mt-7">
+      <div className="flex flex-1 flex-col pb-9">
+        <div className="px-6 py-7">
+          <h1 className="text-left text-[24px] leading-7.5 font-bold tracking-[-1.2px] text-brand-black">
+            하나은행 계좌번호를
+            <br />
+            입력해 주세요.
+          </h1>
+        </div>
+        <div className="mt-9 px-6">
           <AccountNumberInput
             label="계좌번호"
             placeholder="계좌번호"
@@ -34,7 +36,7 @@ const RegisterAccount = ({ onNext }: Props) => {
             onVerify={handleVerifyAccount}
           />
         </div>
-        <div className="mt-auto pt-9">
+        <div className="mt-auto px-6 pt-9">
           <Button size="L" variant="active" onClick={onNext}>
             다음
           </Button>
