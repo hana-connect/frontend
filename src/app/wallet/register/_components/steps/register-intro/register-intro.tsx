89c8f01c5cef9276df40babe25fa9982fcbab686
thisminji <thisminji@dgu.ac.kr>
2026-04-12 14:50:14 +0900

    feat(wallet): 계좌 등록 단계-아이 ui
    글자, 색상 스타일 추가되면 그거 반영해야해요

--- src/app/wallet/register/_components/steps/register-intro/register-intro.tsx
+++ src/app/wallet/register/_components/steps/register-intro/register-intro.tsx
@@ -8,11 +8,31 @@ interface Props {
   onNext: () => void;
 }
 
+type ViewType = "child" | "parent";
+
+const introViewContent: Record<
+  ViewType,
+  { bannerTitle: string; showIrpCard: boolean }
+> = {
+  child: {
+    bannerTitle: "원픽 통장이란?",
+    showIrpCard: false,
+  },
+  parent: {
+    bannerTitle: "IRP의 장점은 무엇인가요?",
+    showIrpCard: true,
+  },
+};
+
 const RegisterIntro = ({ onNext }: Props) => {
+  const viewType: ViewType = "child";
+
+  const { bannerTitle, showIrpCard } = introViewContent[viewType];
+
   return (
     <>
       <Header type="sub" title="계좌 등록하기" />
-      <RegisterInfoBanner title="원픽 통장이란?" />
+      <RegisterInfoBanner title={bannerTitle} />
       <div className="mt-9 px-6">
         <button type="button" onClick={onNext} className="block w-full">
           <Image
@@ -23,9 +43,32 @@ const RegisterIntro = ({ onNext }: Props) => {
           />
         </button>
       </div>
-      <div className="mt-6 px-6">
-        <IrpCard />
-      </div>
+      {showIrpCard && (
+        <div className="mt-6 px-6">
+          <IrpCard />
+        </div>
+      )}
+      {!showIrpCard && (
+        <div className="mt-14.25 px-6">
+          <div className="flex flex-col gap-6 text-left">
+            <div className="flex flex-col gap-2">
+              <h2 className="text-[16px] text-black">충전계좌 등록 안내</h2>
+              <ul className="list-disc pl-6 text-[16px] text-[#555555]">
+                <li>입출금 계좌만 등록할 수 있어요.</li>
+                <li>만 14세 미만은 하나은행 계좌만 등록할 수 있어요.</li>
+              </ul>
+            </div>
+            <div className="flex flex-col gap-2 pt-1">
+              <p className="text-[16px] text-black">계좌가 없다면?</p>
+              <p className="text-[16px] text-[#555555]">
+                아이부자 앱에서{" "}
+                <span className="text-[#B465FF]">비대면 계좌개설</span>로
+                하나은행 계좌를 만들 수 있어요.
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="flex flex-1 flex-col gap-4 p-6"></div>
     </>
   );
