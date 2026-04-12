b35981601436499d415f315a0a8de44f0aca19a7
imddoy <imddoy@duksung.ac.kr>
2026-04-12 22:55:22 +0900

    refactor(root): 키패드 컴포넌트 위치 변경
    상위 페이지에서 호출하는대로 사용할 수 있도록 컴포넌트로 따로 분리했습니다.
    
    HF-32

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -1,58 +1,22 @@
 "use client";
 
-import NumericInputLayout from "@/common/components/keypad/NumericInputLayout";
-import NumericKeypad from "@/common/components/keypad/NumericKeypad";
-import PasswordHeader from "@/common/components/keypad/PasswordHeader";
-import { usePasswordInput } from "@/common/hooks/usePasswordInput";
+import { useRouter } from "next/navigation";
+import Password from "@/common/components/keypad/Password";
 
-export default function PasswordPage() {
-  const {
-    value,
-    errorMessage,
-    keypadItems,
-    isReady,
-    handleDigitPress,
-    handleBackspacePress,
-  } = usePasswordInput({
-    maxLength: 4,
-    onComplete: async (password) => {
-      await new Promise((resolve) => setTimeout(resolve, 300));
-      return password === "1234";
-    },
-  });
+function page() {
+  const router = useRouter();
+
+  const handleAuthSuccess = () => {
+    router.push("/transfer/complete");
+  };
 
   return (
-    <NumericInputLayout
-      top={
-        <PasswordHeader
-          title="계좌 비밀번호를 입력해 주세요"
-          enteredLength={value.length}
-          maxLength={4}
-          errorMessage={errorMessage}
-        />
-      }
-      keypad={
-        isReady ? (
-          <NumericKeypad
-            items={keypadItems}
-            columns={4}
-            onDigitPress={handleDigitPress}
-            onBackspacePress={handleBackspacePress}
-          />
-        ) : (
-          <div className="grid grid-cols-4 justify-items-center gap-x-6 gap-y-7">
-            {Array.from({ length: 16 }, (_, index) => (
-              <div
-                key={`keypad-skeleton-${
-                  // biome-ignore lint/suspicious/noArrayIndexKey: 길이, 순서 고정된 배열이므로 index 사용해도 문제 없음
-                  index
-                }`}
-                className="h-14 w-14"
-              />
-            ))}
-          </div>
-        )
-      }
+    <Password
+      title="계좌 비밀번호를 입력해주세요."
+      onSuccess={handleAuthSuccess}
+      length={4}
     />
   );
 }
+
+export default page;
--- src/app/transfer/amount/page.tsx
+++ src/app/transfer/amount/page.tsx
@@ -1,58 +1,18 @@
 "use client";
 
-import Button from "@/common/components/button/Button";
-import AmountHeader from "@/common/components/keypad/AmountHeader";
-import NumericInputLayout from "@/common/components/keypad/NumericInputLayout";
-import NumericKeypad from "@/common/components/keypad/NumericKeypad";
-import { useAmountInput } from "@/common/hooks/useAmountInput";
+import TransferAmount from "@/common/components/keypad/TransferAmount";
 
-export default function TransferAmountPage() {
-  const {
-    formattedValue,
-    keypadItems,
-    handleDigitPress,
-    handleBackspacePress,
-    handleQuickAdd,
-    numericValue,
-  } = useAmountInput({
-    maxAmount: 800000,
-  });
-
-  const isEnabled = numericValue > 0;
+export default function Page() {
+  const handleNextStep = (amount: number) => {
+    console.log(`${amount}원을 보냅니다.`);
+  };
 
   return (
-    <NumericInputLayout
-      top={
-        <AmountHeader
-          accountHolder="김채현(김채*)"
-          accountNickname="채현이 입출금 (용돈)"
-          balance={800000}
-          amount={formattedValue}
-          onQuickAdd={handleQuickAdd}
-        />
-      }
-      keypad={
-        <NumericKeypad
-          items={keypadItems}
-          columns={3}
-          onDigitPress={handleDigitPress}
-          onBackspacePress={handleBackspacePress}
-        />
-      }
-      bottom={
-        <Button
-          size="L"
-          variant={isEnabled ? "active" : "disabled"}
-          disabled={!isEnabled}
-          onClick={() => {
-            if (isEnabled) {
-              console.log("다음 단계로 이동:", numericValue);
-            }
-          }}
-        >
-          다음
-        </Button>
-      }
+    <TransferAmount
+      accountHolder="김채현(김채*)"
+      accountNickname="채현이 입출금 (용돈)"
+      balance={500000}
+      onNext={handleNextStep}
     />
   );
 }
