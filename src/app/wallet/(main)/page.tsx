72104757b3c424537ede1b008ef855e4072bb378
imddoy <imddoy@duksung.ac.kr>
2026-04-14 17:12:30 +0900

    chore(root): PR 올릴 용도로 파일 임시 제거

--- src/app/wallet/(main)/page.tsx
+++ src/app/wallet/(main)/page.tsx
@@ -1,17 +1,5 @@
-import AccountList from "./_components/AccountList";
-import KidSection from "./_components/KidSection";
-import WalletBalance from "./_components/WalletBalance";
-
 function Page() {
-  return (
-    <main className="px-6 py-4 space-y-6">
-      <WalletBalance />
-
-      <AccountList />
-
-      <KidSection />
-    </main>
-  );
+  return <main className="px-6 py-4 space-y-6"></main>;
 }
 
 export default Page;
