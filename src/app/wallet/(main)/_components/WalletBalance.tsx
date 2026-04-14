99cd3e0822b99383eeee8ad7c3626ff7ed9f19ba
imddoy <imddoy@duksung.ac.kr>
2026-04-14 16:10:40 +0900

    feat(wallet): 지갑 잔액 컴포넌트
    내 지갑 UI
    
    HF-65

--- src/app/wallet/(main)/_components/WalletBalance.tsx
+++ src/app/wallet/(main)/_components/WalletBalance.tsx
@@ -1,5 +1,30 @@
+import { CircleQuestionMark } from "lucide-react";
+import Image from "next/image";
+
 function WalletBalance() {
-  return <div>WalletBalance</div>;
+  const amount = 1234567;
+
+  return (
+    <div className="flex items-center justify-between rounded-2xl py-5">
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center gap-2 text-heading-24-b">
+          <span>내 지갑 잔액</span>
+          <CircleQuestionMark size={18} className="text-[#333]" />
+        </div>
+
+        <span className="text-[32px] text-heading-24-b">
+          {amount.toLocaleString()}원
+        </span>
+      </div>
+      <Image
+        src="/svg/wallet/ic_wallet.svg"
+        alt="wallet"
+        width={90}
+        height={90}
+        className="object-contain"
+      />
+    </div>
+  );
 }
 
 export default WalletBalance;
