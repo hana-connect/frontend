2f97d4ee4ac1683f4281823fb26d28d31eb55bf6
hansoojeongsj <tnwjd3758@gmail.com>
2026-04-14 15:35:15 +0900

    feat(root): 코드 정리 및 mock 분리
    코드 정리 및 mock 분리
    
    HF-53

--- src/app/wallet/saving-letter/page.tsx
+++ src/app/wallet/saving-letter/page.tsx
@@ -3,123 +3,9 @@
 import { useMemo, useState } from "react";
 import Header from "@/common/components/header/Header";
 import PassbookLayout from "@/common/components/passbook-layout/PassbookLayout";
+import PassbookPaper from "@/common/components/passbook-layout/PassbookPaper";
 import { Tabs, TabsList, TabsTrigger } from "@/common/components/tabs/Tabs";
-
-type LetterItem = {
-  id: number;
-  date: string;
-  amount: number;
-  message: string;
-  sender: string;
-  balance: number;
-};
-
-const MOCK_LETTER: LetterItem[] = [
-  {
-    id: 1,
-    date: "20260414",
-    amount: 50000,
-    message: "우리 별돌이 사랑해",
-    sender: "김도이",
-    balance: 600000,
-  },
-  {
-    id: 2,
-    date: "20260410",
-    amount: 50000,
-    message: "말해주면 좋겠어",
-    sender: "김채현",
-    balance: 550000,
-  },
-  {
-    id: 3,
-    date: "20260405",
-    amount: 5000000,
-    message: "무엇보다 건강이",
-    sender: "김도이",
-    balance: 50000000,
-  },
-  {
-    id: 4,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 5,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 6,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 7,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 8,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 9,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 10,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 11,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 12,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-  {
-    id: 13,
-    date: "20260401",
-    amount: 50000,
-    message: "가장 중요하니까 가나다",
-    sender: "이하나",
-    balance: 450000,
-  },
-];
+import { MOCK_ACCOUNT_DATA } from "./_mock/data";
 
 const ITEMS_PER_PAGE = 12;
 
@@ -127,20 +13,22 @@ const LetterPage = () => {
   const [activeTab, setActiveTab] = useState("all");
   const [currentPage, setCurrentPage] = useState(1);
 
+  const accountInfo = MOCK_ACCOUNT_DATA;
+
   const senders = useMemo(() => {
     const uniqueSenders = Array.from(
-      new Set(MOCK_LETTER.map((item) => item.sender)),
+      new Set(accountInfo.history.map((item) => item.sender)),
     );
     return ["all", ...uniqueSenders];
-  }, []);
+  }, [accountInfo.history]);
 
   const filteredData = useMemo(() => {
     const data =
       activeTab === "all"
-        ? MOCK_LETTER
-        : MOCK_LETTER.filter((item) => item.sender === activeTab);
+        ? accountInfo.history
+        : accountInfo.history.filter((item) => item.sender === activeTab);
     return data;
-  }, [activeTab]);
+  }, [activeTab, accountInfo.history]);
 
   const totalPages = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
   const currentData = filteredData.slice(
@@ -156,8 +44,6 @@ const LetterPage = () => {
     if (currentPage < totalPages) setCurrentPage(currentPage + 1);
   };
 
-  const emptyRowsCount = ITEMS_PER_PAGE - currentData.length;
-  const emptyRows = Array.from({ length: emptyRowsCount });
   const tabSection = (
     <Tabs
       defaultValue="all"
@@ -181,8 +67,8 @@ const LetterPage = () => {
       <Header type="sub" title={"지난 작성 내역"} />
 
       <PassbookLayout
-        title="오늘부터, 하나 적금"
-        subTitle="100-2223-444"
+        title={accountInfo.productName}
+        subTitle={accountInfo.accountNumber}
         onPrev={handlePrev}
         onNext={handleNext}
         isFirstPage={currentPage === 1}
@@ -190,26 +76,26 @@ const LetterPage = () => {
         tabs={tabSection}
       >
         <div className="w-full">
-          <div className="flex justify-between border-y border-[#ECECEC] py-1 text-black text-[15px] font-semibold text-left">
+          {/* 테이블 헤더 생략 (동일) */}
+          <div className="flex justify-between border-y border-grey-5 py-1 text-black text-[15px] font-semibold text-left">
             <span className="w-6"></span>
             <span className="w-17">거래일</span>
             <span className="flex-1">메시지</span>
             <span className="w-16">입금액</span>
             <span className="w-18">잔액</span>
           </div>
-          <div className="flex flex-col relative">
-            <div
-              className="absolute top-0 bottom-0 left-[25%] w-[38%] bg-grey-3 z-0"
-              aria-hidden="true"
-            />
 
+          <PassbookPaper
+            currentCount={currentData.length}
+            highlightRangeClass="left-[25%] w-[38%]"
+          >
             {currentData.map((item, index) => (
               <div key={item.id} className="relative z-10">
                 {index !== 0 && index % 6 === 0 && (
                   <div className="h-px w-full bg-grey-5 my-0 shadow-[0_1px_3px_rgba(0,0,0,0.1)]" />
                 )}
-                <div className="flex justify-between items-center py-2 text-[13px] font-medium text-[#2E2E36]">
-                  <span className="w-6 text-center ">
+                <div className="flex justify-between items-center py-2 text-[13px] font-medium text-brand-black">
+                  <span className="w-6 text-center text-grey-6">
                     {String(
                       (currentPage - 1) * ITEMS_PER_PAGE + index + 1,
                     ).padStart(2, "0")}
@@ -221,29 +107,13 @@ const LetterPage = () => {
                   <span className="w-16 text-right">
                     {item.amount.toLocaleString()}
                   </span>
-                  <span className="w-18 text-right ">
+                  <span className="w-18 text-right">
                     {item.balance.toLocaleString()}
                   </span>
                 </div>
               </div>
             ))}
-
-            {emptyRows.map((_, i) => {
-              const rowIndex = currentData.length + i;
-              const rowKey = `empty-row-p${currentPage}-${i}`;
-
-              return (
-                <div key={rowKey} className="relative z-10">
-                  {rowIndex !== 0 && rowIndex % 6 === 0 && (
-                    <div className="h-px w-full bg-grey-5 my-0 shadow-[0_1px_3px_rgba(0,0,0,0.1)]" />
-                  )}
-                  <div className="flex py-2 text-[13px] invisible">
-                    <span className="w-6">&nbsp;</span>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          </PassbookPaper>
         </div>
       </PassbookLayout>
     </div>
